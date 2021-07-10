6c1d7fde34a56061edfe55056d14ceeb55f5f735
Robson <robsonbrazjunior@gmail.com>
2021-07-10 10:07:20 -0300

    Add business domain

---
+++ 02-Features/Features/Core/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Features.Core
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        void Adicionar(TEntity entity);
+        TEntity ObterPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity entity);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+    }
+}
