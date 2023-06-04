740065597d97eb4a0908c62e3637448025e1af3b
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-06-04 10:06:12 -0300

    Adição de novo projeto de testes com novos exemplos de testes automáticos

---
+++ 01_Testes_De_Unidade/Features/Clientes/ClienteEmailNotification.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Features.Clientes
+{
+	public class ClienteEmailNotification : INotification
+	{
+		public string Origem { get; }
+		public string Destino { get; }
+		public string Assunto { get; }
+		public string Mensagem { get; }
+
+		public ClienteEmailNotification(string origem, string destino, string assunto, string mensagem)
+		{
+			Origem = origem;
+			Destino = destino;
+			Assunto = assunto;
+			Mensagem = mensagem;
+		}
+	}
+}
